8299be987f243fd2b7e6f3747909e1576acc443b
mirza2018 <mirza.eee.4th@gmail.com>
2025-04-07 17:50:11 +0600

    add all

--- src/Components/DelarManagementPage/SingleDealerPage/ContractData.jsx
+++ src/Components/DelarManagementPage/SingleDealerPage/ContractData.jsx
@@ -9,7 +9,42 @@ const ContractData = () => {
   return (
     <div className="container mx-auto  border-2 border-secondary-color rounded-md my-20">
       <div className="max-w-[1350px]  mx-auto my-10">
-        <h1 className="font-bold text-6xl">Final note</h1>
+        <h1 className="font-bold text-6xl mb-5">Final note</h1>
+        <section className="flex flex-col mx-5">
+          <h1 className="text-highlight-color text-5xl font-bold mb-3">With</h1>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
+            <p>Model and brand</p>
+            <p>RENAULT, Captur, TCe 90</p>
+          </div>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
+            <p>Bil type</p>
+            <p>Passenger car</p>
+          </div>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
+            <p>Year</p>
+            <p>2017</p>
+          </div>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
+            <p>Kilometer</p>
+            <p>113000</p>
+          </div>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
+            <p>Registration number</p>
+            <p>BZ88778</p>
+          </div>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
+            <p>Fuel</p>
+            <p>Gasoline</p>
+          </div>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
+            <p>The condition of the car</p>
+            <p>Can't drive</p>
+          </div>
+          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-y border-secondary-color p-3 ">
+            <p>Last seen</p>
+            <p>Godkendt, 12. dec. 2023</p>
+          </div>
+        </section>
 
         <main className="flex flex-col lg:grid lg:grid-cols-2  gap-0 mx-6 my-10">
           <section className="flex flex-col ">
@@ -84,74 +119,7 @@ const ContractData = () => {
             </div>
           </section>
         </main>
-        <section className="flex flex-col mx-5">
-          <h1 className="text-highlight-color text-5xl font-bold mb-3">With</h1>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
-            <p>Model and brand</p>
-            <p>RENAULT, Captur, TCe 90</p>
-          </div>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
-            <p>Bil type</p>
-            <p>Passenger car</p>
-          </div>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
-            <p>Year</p>
-            <p>2017</p>
-          </div>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
-            <p>Kilometer</p>
-            <p>113000</p>
-          </div>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
-            <p>Registration number</p>
-            <p>BZ88778</p>
-          </div>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
-            <p>Fuel</p>
-            <p>Gasoline</p>
-          </div>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-t border-secondary-color p-3 ">
-            <p>The condition of the car</p>
-            <p>Can't drive</p>
-          </div>
-          <div className="grid grid-cols-2 bg-base-color border-x font-medium border-y border-secondary-color p-3 ">
-            <p>Last seen</p>
-            <p>Godkendt, 12. dec. 2023</p>
-          </div>
-        </section>
-        <section className="flex flex-col mx-5 ">
-          <h1 className="text-highlight-color text-5xl font-bold mb-3 py-3">
-            Re-registration/de-registration view
-          </h1>
-          <div className=" border rounded-md font-medium border-secondary-color px-3 py-9 flex flex-col gap-9">
-            <div className="flex justify-between items-center w-full ">
-              <h1>Køber omregistrerer/afmelder bilen inden for 4 hverdage*</h1>
-              <p className="w-4 aspect-square rounded-full bg-highlight-color ring ring-highlight-color border-2 border-white"></p>
-            </div>
-            <div className="flex justify-between items-center w-full ">
-              <h1>Buyer and seller re-register the car together</h1>
-              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
-            </div>
-            <div className="flex justify-between items-center w-full ">
-              <h1>
-                The seller deregisters the car and hands over the number plates
-              </h1>
-              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
-            </div>
-            <div className="flex justify-between items-center w-full ">
-              <h1>The car is deregistered</h1>
-              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
-            </div>
-            <div className="flex justify-between items-center w-full ">
-              <h1>
-                *According to Danish legislation, the car must be
-                re-registered/de-registered no later than 4 working days after
-                the transaction
-              </h1>
-              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
-            </div>
-          </div>
-        </section>
+
         <section className="flex flex-col mx-5">
           <div className="text-highlight-color text-5xl font-bold mb-3 py-3">
             Price
@@ -207,6 +175,40 @@ const ContractData = () => {
           </div>
         </section>
 
+        <section className="flex flex-col mx-5 ">
+          <h1 className="text-highlight-color text-5xl font-bold mb-3 py-3">
+            Re-registration/de-registration view
+          </h1>
+          <div className=" border rounded-md font-medium border-secondary-color px-3 py-9 flex flex-col gap-9">
+            <div className="flex justify-between items-center w-full ">
+              <h1>Køber omregistrerer/afmelder bilen inden for 4 hverdage*</h1>
+              <p className="w-4 aspect-square rounded-full bg-highlight-color ring ring-highlight-color border-2 border-white"></p>
+            </div>
+            <div className="flex justify-between items-center w-full ">
+              <h1>Buyer and seller re-register the car together</h1>
+              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
+            </div>
+            <div className="flex justify-between items-center w-full ">
+              <h1>
+                The seller deregisters the car and hands over the number plates
+              </h1>
+              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
+            </div>
+            <div className="flex justify-between items-center w-full ">
+              <h1>The car is deregistered</h1>
+              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
+            </div>
+            <div className="flex justify-between items-center w-full ">
+              <h1>
+                *According to Danish legislation, the car must be
+                re-registered/de-registered no later than 4 working days after
+                the transaction
+              </h1>
+              <p className="w-4 aspect-square rounded-full  ring ring-highlight-color "></p>
+            </div>
+          </div>
+        </section>
+
         <section className=" border rounded-md font-medium border-secondary-color px-3 py-9 flex md:flex-row flex-col justify-around  gap-9 mx-5 my-10 ">
           <div className="flex flex-col  justify-center items-center gap-2">
             <p className=" h-36 aspect-video border-2 border-dotted border-highlight-color rounded-lg"></p>
@@ -258,6 +260,10 @@ const ContractData = () => {
             </button>
           </div>
           <div className="flex  justify-center gap-2">
+            <button className="font-bold text-white bg-highlight-color p-2 rounded-md flex justify-center items-center gap-2">
+     
+              Signature
+            </button>
             <button className="font-bold text-white bg-highlight-color p-2 rounded-md flex justify-center items-center gap-2">
               <TiPrinter className="text-xl" />
               Print
